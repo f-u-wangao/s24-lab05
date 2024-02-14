c84b9dff7e133c9a4b5b6cd6c386c1bd8dd62d93
Otto <aowanow@gmail.com>
2024-02-13 23:39:01 -0500

    implement DelegationSortedIntList

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,49 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList {
+    private int totalAdded = 0;
+    private IntegerList list = new SortedIntList();
 
+    public DelegationSortedIntList() {
+    }
+
+    // Increment the totalAdded count by 1 and reuses the add method from the
+    // SortedIntList class
+    public boolean add(int num) {
+        totalAdded++;
+        return this.list.add(num);
+    }
+
+    // Increment the totalAdded count by the size of the list being added and reuses
+    // the addAll method from the SortedIntList class
+    public boolean addAll(IntegerList list) {
+        totalAdded += list.size();
+        return this.list.addAll(list);
+    }
+
+    // Accessor for the totalAdded count
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
+    // Reuses the get method from the SortedIntList class
+    public int get(int index) {
+        return this.list.get(index);
+    }
+
+    // Reuses the remove method from the SortedIntList class
+    public boolean remove(int num) {
+        return this.list.remove(num);
+    }
+
+    // Reuses the removeAll method from the SortedIntList class
+    public boolean removeAll(IntegerList list) {
+        return this.list.removeAll(list);
+    }
+
+    // Reuses the size method from the SortedIntList class
+    public int size() {
+        return this.list.size();
+    }
 }
